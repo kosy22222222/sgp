b5ef64a105e3da8e9824ff9dbe005a7ce39656f1
kosy22222222 <113009195+kosy22222222@users.noreply.github.com>
2023-05-21 22:56:04 +0900

    리팩토리 ing

--- app/src/main/java/kr/ac/tukorea/kodg2002/sgp05/Cat.java
+++ app/src/main/java/kr/ac/tukorea/kodg2002/sgp05/Cat.java
@@ -1,4 +1,42 @@
 package kr.ac.tukorea.kodg2002.sgp05;
 
-public class Cat {
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import java.util.ArrayList;
+
+import kr.ac.tukorea.kodg2002.sgp05.AnimSprite;
+import kr.ac.tukorea.kodg2002.sgp05.IRecyclable;
+
+public class Cat extends AnimSprite implements IRecyclable {
+
+    public Cat() {
+        super(R.drawable.cat, 2.0f, 3.0f, 2.0f, 3.0f, 8, 1);
+    }
+
+    protected enum State {
+        running, stop, clicked, COUNT
+    }
+
+    private static Rect[] makeRects(int... indices) {
+        Rect[] rects = new Rect[indices.length];
+        for (int i = 0; i < indices.length; i++) {
+            int idx = indices[i];
+            int l = 72 + (idx % 100) * 272;
+            int t = 132 + (idx / 100) * 272;
+            rects[i] = new Rect(l, t, l + 140, t + 140);}
+        return rects;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        long now = System.currentTimeMillis();
+        float time = (now - createdOn) / 1000.0f;
+        Rect[] rects = makeRects(100, 101, 102, 103);
+        int frameIndex = Math.round(time * fps) % rects.length;
+        canvas.drawBitmap(bitmap, rects[frameIndex], dstRect, null);
+    }
+
+    @Override
+    public void onRecycle() {
+    }
 }
--- app/src/main/java/kr/ac/tukorea/kodg2002/sgp05/CatActivity.java
+++ app/src/main/java/kr/ac/tukorea/kodg2002/sgp05/CatActivity.java
@@ -2,17 +2,24 @@ package kr.ac.tukorea.kodg2002.sgp05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import kr.ac.tukorea.kodg2002.sgp05.Cat;
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 import kr.ac.tukorea.kodg2002.sgp05.databinding.ActivityCatBinding;
 
 public class CatActivity extends AppCompatActivity {
     private ActivityCatBinding binding;
+    private final Cat cat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+    public CatActivity() {
+        cat = new Cat();
     }
 }
